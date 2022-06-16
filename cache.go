bf9ac31cc44c56497b46b7bc305658d9b88c0302
Evgenii Klimov <eklmv.spareacc@gmail.com>
2022-06-16 18:39:49 +0300

    task 5.2 done

--- cache.go
+++ cache.go
@@ -3,21 +3,49 @@ package cache
 import "time"
 
 type Cache struct {
+	storage map[string]cacheEntry
+}
+
+type cacheEntry struct {
+	value    string
+	deadline time.Time
+}
+
+func (copy cacheEntry) isExpired() bool {
+	if copy.deadline.IsZero() || time.Until(copy.deadline) > 0 {
+		return false
+	} else {
+		return true
+	}
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (self *Cache) Get(key string) (string, bool) {
+	entry, ok := self.storage[key]
+	if ok || !entry.isExpired() {
+		return entry.value, ok
+	} else {
+		return "", ok
+	}
 }
 
-func (receiver) Put(key, value string) {
+func (self *Cache) Put(key, value string) {
+	self.storage[key] = cacheEntry{value: value}
 }
 
-func (receiver) Keys() []string {
+func (self *Cache) Keys() []string {
+	keys := make([]string, 0, len(self.storage))
+	for key, entry := range self.storage {
+		if !entry.isExpired() {
+			keys = append(keys, key)
+		}
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (self *Cache) PutTill(key, value string, deadline time.Time) {
+	self.storage[key] = cacheEntry{value: value, deadline: deadline}
 }
